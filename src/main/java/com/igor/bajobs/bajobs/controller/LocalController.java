a95b2b34ef0531a16bfb118687f3e5a3d58f2560
André Igor <andreigormenezes@gmail.com>
2023-08-27 22:06:22 -0300

    added GetMapping

--- src/main/java/com/igor/bajobs/bajobs/controller/LocalController.java
+++ src/main/java/com/igor/bajobs/bajobs/controller/LocalController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ public class LocalController {
         this.vagaService = vagaService;
     }
     
+    @GetMapping()
     public ResponseEntity<List<String>> getLocais() {
 
         return ResponseEntity.ok(vagaService.getAllLocais());
